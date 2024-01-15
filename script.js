c3f7cbb2acc2a8ef022083291c361497ebf6f075
usernameB99 <kum-a-saufn@live.at>
2024-01-15 18:43:43 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,58 @@
+mySubmit.onclick = function(){
+
+const ammountToMake = Number(document.getElementById("ammountToMake").value);
+const aromaPercentage = Number(document.getElementById("aromaPercentage").value);
+const desiredNicotineStrength = Number(document.getElementById("desiredNicotineStrength").value);
+const baseNicotineStrength = Number(document.getElementById("baseNicotineStrength").value);
+
+const mySubmit = document.getElementById("mySubmit");
+const result = document.getElementById("myResult");
+
+let aroma;
+let nicBase;
+let zeroBase;
+
+
+    if(ammountToMake === 0 || ammountToMake === null){
+        result.textContent = "Ammount of Liquid must be greater than 0!"
+    } else {
+
+              if(aromaPercentage === 0 || aromaPercentage === null){                                                     // without aroma
+
+                if(baseNicotineStrength === 0 || baseNicotineStrength === null){
+                    result.textContent = "you must type in nicotine strength from nicotine shot that is grater than 0!"
+                } else{
+                    
+                    nicBase = (desiredNicotineStrength * ammountToMake) / baseNicotineStrength;
+                    zeroBase = ammountToMake - nicBase;
+                    result.textContent = `${zeroBase}ml basis + ${nicBase}ml Nikotin shot`;
+                }
+
+                } else if(desiredNicotineStrength === 0 || desiredNicotineStrength === null){                             // without nicotine
+                    
+                    if(aromaPercentage === 0 || aromaPercentage === null){
+                        result.textContent = "you must type in aroma percentage that is greater than 0!"
+                    } else{
+                        aroma = (ammountToMake * aromaPercentage) / 100;
+                        zeroBase = ammountToMake - aroma;
+                        result.textContent = `${aroma}ml aroma + ${zeroBase}ml basis`;
+                    }
+                    
+                } else{                                                                                                   // do all calculations
+
+                    if(baseNicotineStrength === 0 || baseNicotineStrength === null){
+                        result.textContent = "you must type in nicotine strength from nicotine shot that is grater than 0!!"
+                    } else{
+
+                        aroma = (ammountToMake * aromaPercentage) / 100;
+                
+                        nicBase = (desiredNicotineStrength * ammountToMake) / baseNicotineStrength;
+                
+                        zeroBase = ammountToMake - nicBase - aroma;
+                
+                        result.textContent = `${aroma}ml aroma + ${zeroBase}ml basis + ${nicBase}ml Nikotin shot`;
+                    }
+
+                }
+    }
+}
